1c40dc23be5c5725ccd39c5108f573749f5db145
Delyc <d.twizeyima@alustudent.com>
2022-10-03 07:59:50 +0200

    about blog oneblog product and home pages

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        gradient1:"#D798E1",
+        gradient2:"#9BFFA5",
+        gradient3:"#AED3FF",
+        headingColor: "#1D2130",
+        paraColor: "#1D2130",
+        textGradient1: "#373FFF",
+        textGradient2: "#3ACAF8",
+        lightViolet: "#E0E4FC",
+        anotherBg:"#D798E1",
+        borderColor: "#6199ED"
+
+
+
+      }
+    },
+  },
+  plugins: [],
+}
