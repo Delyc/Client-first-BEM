9065f0ff08bfb9d14264d633f254455753091ba5
Delyc <d.twizeyima@alustudent.com>
2022-10-03 12:07:59 +0200

    applying changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,16 @@ module.exports = {
         gradient1:"#D798E1",
         gradient2:"#9BFFA5",
         gradient3:"#AED3FF",
-        headingColor: "#1D2130",
-        paraColor: "#1D2130",
+        "blue-dark": "#1D2130",
+        "gray-light": "#1D2130",
         textGradient1: "#373FFF",
         textGradient2: "#3ACAF8",
         lightViolet: "#E0E4FC",
-        anotherBg:"#D798E1",
         borderColor: "#6199ED"
-
-
-
-      }
+      },
+     fontFamily:{
+      roboto: "Roboto"
+     }
     },
   },
   plugins: [],
